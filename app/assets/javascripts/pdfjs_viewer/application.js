ed4cf391fa1600fd4cc0ad032ff902fc36125a42
Marek de Heus <marek@deheus.net>
2024-09-19 22:11:36 +0200

    Remove pdfjs from sprockets/assets as we load it through esbuild/package.json

--- app/assets/javascripts/pdfjs_viewer/application.js
+++ app/assets/javascripts/pdfjs_viewer/application.js
@@ -9,7 +9,3 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require pdfjs_viewer/pdfjs/pdf
-//= require pdfjs_viewer/viewer_configurations
-//= require pdfjs_viewer/viewer
